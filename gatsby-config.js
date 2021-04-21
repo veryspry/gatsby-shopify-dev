57aa9d41d618cf4a1326a3de6e4b5bdaf4bca375
veryspry <ehlinger.matt@gmail.com>
2021-04-21 13:59:25 -0500

    fix: make env variables consistent with cloud integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,11 +16,11 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: 'gatsby-source-shopify-experimental',
+      resolve: 'gatsby-source-shopify',
       options: {
-        apiKey: process.env.SHOPIFY_ADMIN_API_KEY,
-        password: process.env.SHOPIFY_ADMIN_PASSWORD,
-        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+        apiKey: process.env.SHOPIFY_API_KEY,
+        password: process.env.SHOPIFY_SHOP_PASSWORD,
+        storeUrl: process.env.SHOPIFY_STORE_URL,
         downloadImages: true,
       },
     },
