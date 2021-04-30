37b7dfc971bf942a2b2e0bd392935c3ac173bff4
veryspry <ehlinger.matt@gmail.com>
2021-04-30 10:21:58 -0500

    feat: prefix shopify store url with GATSBY_ to expose it at runtime

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: "gatsby-source-shopify",
       options: {
-        apiKey: process.env.SHOPIFY_API_KEY,
+        apiKey: process.env.GATSBY_SHOPIFY_API_KEY,
         password: process.env.SHOPIFY_SHOP_PASSWORD,
         storeUrl: process.env.SHOPIFY_STORE_URL,
         shopifyConnections: ["collections"],
